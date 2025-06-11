1403946f06380bdd32a113836b542bae8d8f4e5c
AliZarKazmi <alizarkazmi55@gmail.com>
2025-06-11 23:31:31 +0000

    keras fundamentals

--- DeepLearning/KerasWorkflow.py
+++ DeepLearning/KerasWorkflow.py
@@ -81,6 +81,7 @@ model.add(Dense(100, activation = 'relu'))
 #the last layer has 1 node which is the Output layer / adding output layer
 model.add(Dense(1))
 
+# Compile the model
 model.compile(optimizer = 'adam', loss ='mean_squared_error')
 
 
@@ -89,5 +90,111 @@ model.compile(optimizer = 'adam', loss ='mean_squared_error')
                 3. Fit a Model
 
             Q:What is fitting a Model:
-            Ans: That is applying back-propagation and 
+            Ans: That is applying back-propagation and gradient descent with your data to update the weights    
+            The fit step looks similar to what you have seen in scikit-learn though it has more options.
+
+            ***Scaling data before fitting can ease optimization
 '''
+
+n_cols = predictors.shape[1]
+
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(100, activation='relu', input_shape = (n_cols,)))
+
+model.add(Dense(100, activation = 'relu'))
+
+#the last layer has 1 node which is the Output layer / adding output layer
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer = 'adam', loss ='mean_squared_error')
+
+# Fit the model 
+model.fit(predictors, target)
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----Classification Models in Deep Learning
+
+''''
+    For Classification we do couple of things differently , the biggest change are : first you set the loss function as 'categorical_crossentrophy' instead of 'mean_squared_error'.
+    This is not the only possible loss function for classification problems, but it is by far the most common.
+
+    ***FOR categorical crossentropy loss function, a lower score is better , but it is still hard to interpret. So I've added this argument "metrics equal accuracy".
+
+    "metrics equal accuracy" : this means I want to print out the accuracy score at the end of each epoch, which makes it easier to see and understand the models progress.
+
+    **The second thing we do is you to modify the last layer, so it has a separate node for each potential outcome. You also change
+    the activation function to "softmax".
+
+    'softmax ' activation function ensures the predictions sum to 1 , so they can be interpreted as probabilities. 
+
+    ***Key points to remember which are different for Classification:
+    1."categorical_crossentrophy" loss function
+    2. Similar to log loss i.e Lower is better
+    3. Add "metrics = ['accuracy'] to compile step for easy-to understand diagnostics.
+    4. Output layer has separate node for each possible outcome, and uses "softmax" activation.
+
+
+Remember : In general we want to convert categoricals in Keras to a format with a separate column for each output.  
+'''
+
+#           ->Code for Classification
+
+#we use 'to_categorical' which is used to convert the data from 1 column to multiple columns.
+from tensorflow.keras.utils import to_categorical
+
+data = pd.read_csv('basketball_shot_log.csv')
+
+predictors = data.drop(['shot_result'], axis = 1 ).values
+
+# Convert the target to categorical: target
+target = to_categorical(data['short_result'])
+
+model = Sequential()
+
+model.add(Dense(100, activation = 'relu', input_shape = (n_cols ,)))
+
+model.add(Dense(100, activation = 'relu'))
+
+model.add(Dense(100, activation = 'relu'))
+
+# output layer for Cassification
+model.add(Dense(2, activation = 'softmax'))
+
+# Compile the model
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Fit the model
+model.fit(predictors, target)
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+''''
+            ->Using Model
+        The things you will want to do in order to use these models are
+        1. "Save" a model after you have trained it
+        2. "Reload" that model
+        3. "Make Predictions" with that model
+
+Code is below
+'''
+
+from tensorflow.keras.models import load_model
+
+#models are saved in format called 'hdf5', for which h5 is common extension
+model.save('model_file.h5')
+
+#Then load the model back into memory with the laod_model function
+my_model = load_model('model_file.h5')
+
+# Calculate predictions: predictions
+predictions = my_model.predict(data_to_predict_with)
+
+
+# Calculate predicted probability of the target column
+probability_true = predictions[:,1]
+
+#verify model structure, we can print out a summary of the model architecture with the summary method.
+my_model.summary()
